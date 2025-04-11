e2a2c0330c636983125bf9635acee8bbb2916627
nachodomna <idomnanovich@mail.austral.edu.ar>
2025-04-11 10:52:38 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,6 +1,6 @@
 def slice_simple():
     texto = "Awesome"
     medio = int(len(texto)/2)
-    print(texto[0:3])
-    print(texto[medio-1;medio+2])
-    print(texto([0:4] + texto[-3:]))
+    print(texto [0:3])
+    print(texto [medio-1;medio+2])
+    print(texto)
